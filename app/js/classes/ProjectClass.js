763adcbf2233e3e66172796b99acb854c3015427
James Garner <james@jadaradix.com>
2014-07-13 14:55:12 +0300

    Rename to ProjectClass. Name consistently.

--- app/js/classes/ProjectClass.js
+++ app/js/classes/ProjectClass.js
@@ -1,62 +1,62 @@
-function DSGM_Project() {
+function ProjectClass() {
 	var _self = this;
-	
+
 	this.name = "";
-	
+
 	this.sprites = [];
 	this.backgrounds = [];
 	this.objects = [];
 	this.rooms = [];
 	this.sounds = [];
-	
+
 	this.addSprite = function(name) {
-		_self.sprites.push(new DSGM_Sprite(name));
+		_self.sprites.push(new SpriteClass(name));
 		_self.refreshResourcesList("sprites");
 		return _self.sprites.length - 1;
 	}
-	
+
 	this.addBackground = function(name) {
-		_self.backgrounds.push(new DSGM_Background(name));
+		_self.backgrounds.push(new BackgroundClass(name));
 		_self.refreshResourcesList("backgrounds");
 		return _self.backgrounds.length - 1;
 	}
-	
+
 	this.addObject = function(name) {
-		_self.objects.push(new DSGM_Object(name));
+		_self.objects.push(new ObjectClass(name));
 		_self.refreshResourcesList("objects");
 		return _self.objects.length - 1;
 	}
-	
+
 	this.addRoom = function(name) {
-		_self.rooms.push(new DSGM_Room(name));
+		_self.rooms.push(new RoomClass(name));
 		_self.refreshResourcesList("rooms");
 		return _self.rooms.length - 1;
 	}
-	
+
 	this.addSound = function(name) {
-		_self.sounds.push(new DSGM_Sound(name));
+		_self.sounds.push(new SoundClass(name));
 		_self.refreshResourcesList("sounds");
 		return _self.sounds.length - 1;
 	}
-	
+
 	this.removeResource = function(resource) {
-		if(resource instanceof DSGM_Sprite) {
+		if(resource instanceof SpriteClass) {
 			_self.sprites.splice(_self.sprites.indexOf(resource), 1);
 			_self.refreshResourcesList("sprites");
 		}
-		else if(resource instanceof DSGM_Background) {
+		else if(resource instanceof BackgroundClass) {
 			_self.backgrounds.splice(_self.backgrounds.indexOf(resource), 1);
 			_self.refreshResourcesList("backgrounds");
 		}
-		else if(resource instanceof DSGM_Object) {
+		else if(resource instanceof ObjectClass) {
 			_self.objects.splice(_self.objects.indexOf(resource), 1);
 			_self.refreshResourcesList("objects");
 		}
-		else if(resource instanceof DSGM_Room) {
+		else if(resource instanceof RoomClass) {
 			_self.rooms.splice(_self.rooms.indexOf(resource), 1);
 			_self.refreshResourcesList("rooms");
 		}
-		else if(resource instanceof DSGM_Sound) {
+		else if(resource instanceof SoundClass) {
 			_self.sounds.splice(_self.sounds.indexOf(resource), 1);
 			_self.refreshResourcesList("sounds");
 		}
@@ -64,7 +64,7 @@ function DSGM_Project() {
 			console.log("Trying to remove unrecognised resource.");
 		}
 	}
-	
+
 	this.refreshResourcesList = function(resourceType) {
 		if(!resourceType || resourceType == "sprites") {
 			$('[data-role="sprites-list"]').empty();
@@ -72,28 +72,28 @@ function DSGM_Project() {
 				$('[data-role="sprites-list"]').append('<li><span data-icon="sprite"><span>' + sprite.name + "</span></span></li>");
 			});
 		}
-		
+
 		if(!resourceType || resourceType == "backgrounds") {
 			$('[data-role="backgrounds-list"]').empty();
 			$.each(project.backgrounds, function(index, background) {
 				$('[data-role="backgrounds-list"]').append('<li><span data-icon="background"><span>' + background.name + "</span></span></li>");
 			});
 		}
-		
+
 		if(!resourceType || resourceType == "objects") {
 			$('[data-role="objects-list"]').empty();
 			$.each(project.objects, function(index, object) {
 				$('[data-role="objects-list"]').append('<li><span data-icon="object"><span>' + object.name + "</span></span></li>");
 			});
 		}
-		
+
 		if(!resourceType || resourceType == "rooms") {
 			$('[data-role="rooms-list"]').empty();
 			$.each(project.rooms, function(index, room) {
 				$('[data-role="rooms-list"]').append('<li><span data-icon="room"><span>' + room.name + "</span></span></li>");
 			});
 		}
-		
+
 		if(!resourceType || resourceType == "sounds") {
 			$('[data-role="sounds-list"]').empty();
 			$.each(project.sounds, function(index, sound) {
@@ -101,43 +101,44 @@ function DSGM_Project() {
 			});
 		}
 	}
-	
+
 	this.load = function(project) {
 		var tempProject = JSON.parse(project);
 		for(var property in tempProject) _self[property] = tempProject[property];
 		_self.refreshResourcesList();
 	}
-	
+
 	this.save = function() {
 		return JSON.stringify(_self);
 	}
+
 }
 
-var DSGM_Resource = {
+var resourcePrototype = {
 	name: "New Resource",
 };
 
-function DSGM_Sprite(name) {
+function SpriteClass(name) {
 	if(name) this.name = name;
 }
-DSGM_Sprite.prototype = Object.create(DSGM_Resource);
+SpriteClass.prototype = Object.create(resourcePrototype);
 
-function DSGM_Background(name) {
+function BackgroundClass(name) {
 	if(name) this.name = name;
 }
-DSGM_Background.prototype = Object.create(DSGM_Resource);
+BackgroundClass.prototype = Object.create(resourcePrototype);
 
-function DSGM_Object(name) {
+function ObjectClass(name) {
 	if(name) this.name = name;
 }
-DSGM_Object.prototype = Object.create(DSGM_Resource);
+ObjectClass.prototype = Object.create(resourcePrototype);
 
-function DSGM_Room(name) {
+function RoomClass(name) {
 	if(name) this.name = name;
 }
-DSGM_Room.prototype = Object.create(DSGM_Resource);
+RoomClass.prototype = Object.create(resourcePrototype);
 
-function DSGM_Sound(name) {
+function SoundClass(name) {
 	if(name) this.name = name;
 }
-DSGM_Sound.prototype = Object.create(DSGM_Sound);
+SoundClass.prototype = Object.create(SoundClass);
