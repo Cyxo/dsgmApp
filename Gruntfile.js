7dc8379ee3b0260fceefa43dc17b0689b127353a
James Garner <james@jadaradix.com>
2014-09-25 02:14:16 +0100

    Improve cross platform compatibility in Grunt. Remove stats Grunt directive.

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,37 +8,46 @@ module.exports = function(grunt) {
       wipeBuild: {
         command: 'rm -rf build'
       },
-      build: {
+      startBuild: {
+        command: 'mkdir build'
+      },
+      endBuild: {
         command: [
-          'mkdir build',
-          'for i in $(find * -type d -maxdepth 0 | grep -viw "node_modules\\|build\\|sass"); do cp -rf $i build/$i ; done',
           'cp index.html build/index.html',
           'sass sass/myApplication.scss:build/myApplication.css --cache-location "sass/cache" --style compressed',
         ].join("&&")
-      },
-      stats: {
-        command: [
-          'rm build/css/myApplication.css',
-          'rm -rf build/css/sass-cache',
-          'rm -rf build/css/fonts',
-          'rm build/js/*.*',
-          'rm -rf build/img',
-        ].join("&&")
+      }
+    },
+
+    copyto: {
+      main: {
+        files: [{
+          src: ['*/**'],
+          dest: 'build',
+          expand: true
+        }],
+        options: {
+          ignore: [
+            'build',
+            'node_modules',
+            'node_modules/**/*',
+            'sass',
+            'sass/**/*'
+          ]
+        }
       }
     }
+
   });
 
   grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-copy-to');
 
   grunt.registerTask('default', [
     'shell:wipeBuild',
-    'shell:build',
-  ]);
-
-  grunt.registerTask('stats', [
-    'shell:wipeBuild',
-    'copy:build',
-    'shell:stats'
+    'shell:startBuild',
+    'copyto:main',
+    'shell:endBuild',
   ]);
 
   grunt.registerTask('clean', [
